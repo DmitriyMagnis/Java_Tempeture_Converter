58298f21072a0a517742dae15c28949b27da08d1
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-10-30 11:56:57 +0100

    ADD/Fahrenheit to celsius convertion

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,20 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("App for temperature converting." );
+
+        double frh = 68;
+
+        double celsius = convFrhToCel(frh);
+
+        System.out.println("Result is celsius: " + celsius);
+
+    }
+
+    private static double convFrhToCel(double frh) {
+        return ((frh - 32) * 5) / 9;
     }
 
 }
